3bfa4b8ac12e51aef6d3ca78ca1eaabbbbd8dbd2
Adam Januszewski <Adpjan@att.net>
2022-07-20 03:19:36 -0400

    Initial Commit

---
+++ TweetProject/TweetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetProjectApp.swift
+//  TweetProject
+//
+//  Created by adam janusewski on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TweetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
