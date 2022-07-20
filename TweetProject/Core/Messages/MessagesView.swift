dca71bb2bb82f8a5fd8e34c9c6dce2f1b7f4af53
Adam Januszewski <Adpjan@att.net>
2022-07-20 13:30:57 -0400

    Messages View created

---
+++ TweetProject/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TweetProject
+//
+//  Created by adam janusewski on 7/20/22.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
