ffb890b568ba78d7960fb20cf4b8441a0c2b24ec
Adam Januszewski <Adpjan@att.net>
2022-07-20 03:23:27 -0400

    first commit

---
+++ TweetProject/Model/Tweet.swift
@@ -0,0 +1,8 @@
+//
+//  Tweet.swift
+//  TweetProject
+//
+//  Created by adam janusewski on 7/20/22.
+//
+
+import Foundation
---
+++ TweetProject/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  TweetProject
+//
+//  Created by adam janusewski on 7/20/22.
+//
+
+import Foundation
