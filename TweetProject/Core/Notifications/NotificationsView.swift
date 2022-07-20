1bb3f6a98de91f18280d8c0d56672e4e8d4ff726
Adam Januszewski <Adpjan@att.net>
2022-07-20 13:29:08 -0400

    Notifications have been made

---
+++ TweetProject/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TweetProject
+//
+//  Created by adam janusewski on 7/20/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
